beaec08532431309f3905faa1bef9e7406cc5369
Bagas Abisena Swastanto <bagasabisena@gmail.com>
2018-05-18 13:17:35 +0700

    use absolute dir to pyenv_virtual_env path

--- conf.d/pyenv-virtualenv.fish
+++ conf.d/pyenv-virtualenv.fish
@@ -1,5 +1,4 @@
-set -l pyenv_virtualenv_path (string split -r -m2 '/' (realpath (type -p pyenv-virtualenv-init)))
+set -l pyenv_virtualenv_path $HOME/.pyenv/plugins/pyenv-virtualenv
 set -x PATH "$pyenv_virtualenv_path[1]/shims" $PATH;
 set -x PYENV_VIRTUALENV_INIT 1;
-
 
